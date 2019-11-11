b0ba35e2f7c932ed06f2b1b72f06545134505a9e
Stefan <lazeastefan@gmail.com>
2019-11-11 16:17:04 +0200

    JsonParser - init

---
+++ DamApp/app/src/main/java/eu/ase/ro/damapp/network/JsonParser.java
@@ -0,0 +1,23 @@
+package eu.ase.ro.damapp.network;
+
+import java.util.List;
+
+import eu.ase.ro.damapp.util.Player;
+
+public class JsonParser {
+    public static HttpResponse parseJson(String js) {
+        return null;
+    }
+
+    private static List<Item> getItemListFromJson() {
+        return null;
+    }
+
+    private static Item getItemFromJson() {
+        return null;
+    }
+
+    private static Player getPlayerInfoFromJson() {
+        return null;
+    }
+}
