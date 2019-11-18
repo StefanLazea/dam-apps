b375c261397551013de31d50753c9434c78c7d19
Stefan <lazeastefan@gmail.com>
2019-11-18 16:03:57 +0200

    Json parser done

--- DamApp/app/src/main/java/eu/ase/ro/damapp/fragment/TransferMarketFragment.java
+++ DamApp/app/src/main/java/eu/ase/ro/damapp/fragment/TransferMarketFragment.java
@@ -1,23 +1,42 @@
 package eu.ase.ro.damapp.fragment;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import eu.ase.ro.damapp.R;
 import eu.ase.ro.damapp.network.HttpManager;
+import eu.ase.ro.damapp.network.HttpResponse;
+import eu.ase.ro.damapp.network.Item;
+import eu.ase.ro.damapp.network.JsonParser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TransferMarketFragment extends Fragment {
     private static final String URL = "http://api.myjson.com/bins/16f54z";
+    private HttpResponse httpResponse;
+
+    private Button btnGoalkeeper;
+    private Button btnCenter;
+    private Button btnInter;
+    private Button btnWinger;
+
+    private ListView lvResponse;
+    private List<Item> selectedResponse = new ArrayList<>();
+
 
     public TransferMarketFragment() {
         // Required empty public constructor
@@ -30,14 +49,100 @@ public class TransferMarketFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_transfer_market, container, false);
 
+        initComponents(view);
+
         new HttpManager() {
             @Override
             protected void onPostExecute(String s) {
-                Toast.makeText(getContext(), s, Toast.LENGTH_LONG).show();
+                httpResponse = JsonParser.parseJson(s);
+                if (httpResponse != null) {
+                    Toast.makeText(getContext(),  httpResponse.toString(), Toast.LENGTH_LONG).show();
+                }
             }
         }.execute(URL);
+
 
         return view;
     }
 
+    private void initComponents(View view) {
+        btnGoalkeeper = view.findViewById(R.id.transfer_market_btn_goalkeeper);
+        btnCenter = view.findViewById(R.id.transfer_market_btn_center);
+        btnInter = view.findViewById(R.id.transfer_market_btn_inter);
+        btnWinger = view.findViewById(R.id.transfer_market_btn_winger);
+
+        lvResponse = view.findViewById(R.id.transfer_market_lv_response);
+
+        ArrayAdapter<Item> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, selectedResponse);
+        //atasare adapter
+
+        lvResponse.setAdapter(adapter);
+        unSelectedButtons();
+
+        //btn goalkeeper wvent
+
+        btnGoalkeeper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (httpResponse != null && httpResponse.getGoalkeeper() != null) {
+                    //adaugare pe listview
+                    unSelectedButtons();
+                    selectButtonColor(btnGoalkeeper);
+                    selectResponse(httpResponse.getGoalkeeper());
+                }
+
+            }
+        });
+
+        btnCenter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (httpResponse != null && httpResponse.getCenter() != null) {
+                    unSelectedButtons();
+                    selectButtonColor(btnCenter);
+                    selectResponse(httpResponse.getCenter());
+                }
+            }
+        });
+
+        btnInter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (httpResponse != null && httpResponse.getInter() != null) {
+                    unSelectedButtons();
+                    selectButtonColor(btnInter);
+                    selectResponse(httpResponse.getInter());
+                }
+            }
+        });
+
+        btnWinger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (httpResponse != null && httpResponse.getWinger() != null) {
+                    unSelectedButtons();
+                    selectButtonColor(btnWinger);
+                    selectResponse(httpResponse.getWinger());
+                }
+            }
+        });
+    }
+
+    private void selectResponse(List<Item> list) {
+        selectedResponse.clear();
+        selectedResponse.addAll(list);
+        ArrayAdapter<Item> adapter = (ArrayAdapter<Item>) lvResponse.getAdapter();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void unSelectedButtons(){
+        btnGoalkeeper.setBackgroundColor(Color.GRAY);
+        btnWinger.setBackgroundColor(Color.GRAY);
+        btnInter.setBackgroundColor(Color.GRAY);
+        btnCenter.setBackgroundColor(Color.GRAY);
+    }
+
+    private void selectButtonColor(Button button){
+        button.setBackgroundColor(Color.GREEN);
+    }
 }
--- DamApp/app/src/main/java/eu/ase/ro/damapp/network/JsonParser.java
+++ DamApp/app/src/main/java/eu/ase/ro/damapp/network/JsonParser.java
@@ -1,23 +1,91 @@
 package eu.ase.ro.damapp.network;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import eu.ase.ro.damapp.AddPlayerActivity;
 import eu.ase.ro.damapp.util.Player;
 
 public class JsonParser {
-    public static HttpResponse parseJson(String js) {
+    public static HttpResponse parseJson(String json) {
+        if (json == null) {
+            return null;
+        }
+
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            List<Item> goalkeeper = getItemListFromJson(jsonObject
+                    .getJSONArray("goalkeeper"));
+            List<Item> inter = getItemListFromJson(jsonObject
+                    .getJSONArray("inter"));
+            List<Item> winger = getItemListFromJson(jsonObject
+                    .getJSONArray("winger"));
+            List<Item> center = getItemListFromJson(jsonObject
+                    .getJSONArray("center"));
+
+
+            return new HttpResponse(goalkeeper, center, inter, winger);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
         return null;
     }
 
-    private static List<Item> getItemListFromJson() {
-        return null;
+    private static List<Item> getItemListFromJson(JSONArray array) throws JSONException {
+        if (array == null) {
+            return null;
+        }
+
+        List<Item> results = new ArrayList<>();
+
+        for (int i = 0; i < array.length(); i++) {
+            Item item = getItemFromJson(array.getJSONObject(i));
+            if (item != null) {
+                results.add(item);
+            }
+        }
+
+        return results;
     }
 
-    private static Item getItemFromJson() {
-        return null;
+    private static Item getItemFromJson(JSONObject object) throws JSONException {
+        if (object == null) {
+            return null;
+        }
+        String team = object.getString("team");
+        String extraInfo = object.getString("extraInfo");
+        PlayerInfo player = getPlayerInfoFromJson(object.getJSONObject("playerInfo"));
+        return new Item(team, extraInfo, player);
     }
 
-    private static Player getPlayerInfoFromJson() {
-        return null;
+    private static PlayerInfo getPlayerInfoFromJson(JSONObject object) throws JSONException {
+        if (object == null) {
+            return null;
+        }
+
+        String name = object.getString("name");
+        Date birthday = null;
+
+        try {
+            birthday = new SimpleDateFormat(AddPlayerActivity.DATE_FORMAT, Locale.ENGLISH).parse(object.getString("birthday"));
+        } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+
+        String favHand = object.getString("favoriteHand");
+
+
+        return new PlayerInfo(name, birthday, favHand);
     }
 }
